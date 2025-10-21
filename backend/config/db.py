b5f3b96048e3b621fb1c0d7ba277d6d09a6b2e68
CLOWREX <ariefadiwibowo2@gmail.com>
2025-10-21 10:27:09 +0700

    update di bagian teacher

--- backend/config/db.py
+++ backend/config/db.py
@@ -1,15 +1,14 @@
+# ini untuk menghubungkan ke database PostgreSQL menggunakan SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Ganti sesuai kredensial PostgreSQL kamu di pgAdmin
 DATABASE_URL = "postgresql+psycopg2://postgres:postgres@localhost:5432/absensi_qr"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Dependency (dipakai di route)
 def get_db():
     db = SessionLocal()
     try:
